ba02e2016692f3a66a580cc06edd23c9ae7333de
TheN008 <suyogkhanal4@gmail.com>
2018-05-19 02:23:52 -0400

    Added notes../

--- 1_map.js
+++ 1_map.js
@@ -6,12 +6,36 @@
         {"name": "Nabin", "age": 19}
     ];
 
+    /*
+        From the people array of object extract all the people's name
+        You pass the function to map method that returns array name
+        But how does that work?
+    */
     let isAdult = function(n) {
-        return n.age>=18?true:false;
+        return n.name;
     }
 
-    people.map(isAdult);
 
-    console.log(people.length);
+    people = people.map(isAdult);
+
+    /*
+        Do you see that isAdult is a function that takes a parameter named n and returns name attribute of it?
+
+        When map is called on people it passes every element individually to isAdult function and makes an
+        array of element of whatever that is returned by the isAdult function.
+
+
+        Basically this is what map is doing:
+
+            function map(func){
+                new_arr = [];
+                for(let i = 0; i<people.length; i++){
+                    let element = func(people[i]);
+                    new_arr.push(element);
+                }
+                return new_arr;
+            }
+    */
+    console.log(people);
 
 })();
