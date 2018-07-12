00348f9cd04d13cc0c19f598fda2a8ea754e21f6
TheN008 <suyogkhanal4@gmail.com>
2018-07-12 15:39:33 +0530

    fixed logic

--- 1_map.js
+++ 1_map.js
@@ -11,19 +11,18 @@
         You pass the function to map method that returns array name
         But how does that work?
     */
-    let isAdult = function(n) {
-        if(n.age >= 18) { 
-            return n.name;
-        }
+    let getNames = function(n) {
+        return n.name;
     }
 
 
-    people = people.map(isAdult);
+    people = people.map(getNames);
 
     /*
-        Do you see that isAdult is a function that takes a parameter named n and returns name attribute of it?
+        Do you see that getNames is a function that takes a parameter named n and returns name attribute of it?
+      
 
-        When map is called on people it passes every element individually to isAdult function and makes an
+        When map is called on people it passes every element individually to getNames function and makes an
         array of element of whatever that is returned by the isAdult function.
 
 
